61350b7e8d48c9846accc3810e460a61567cfcdd
Gabriel ribeiro <mazurco066@gmail.com>
2017-09-28 22:18:29 -0300

    Fixed Music Features

--- commands/play.js
+++ commands/play.js
@@ -14,33 +14,8 @@ exports.run = (bot , message, args, servers, play, position, incrementPosition, 
       var server = servers[message.guild.id];
       server.queue.push(args[1]); //coloca a musica na fila
       incrementPosition();
-      position++;
 
-      const Discord = require('discord.js');
-      if (position > 1){
-
-        YTDL.getInfo(server.queue[0], function(err, info) {
-          const title = info.title;
-          const duration = info.length_seconds;
-          const URL = info.video_url;
-          const uploader = info.author.name;
-          const thumb = info.thumbnail_url;
-          console.log(`${message.author.username}, Tocando a Musica: '${title}.'`);
-          //Trocar essa porra aqui por um Embeed
-          const embed = new Discord.RichEmbed()
-            .setColor('#8bc34a')
-            .setTimestamp()
-            .setTitle(title)
-            .setThumbnail(thumb)
-            .addField('Uploaded by:', uploader, true)
-            .addField('Duration:', duration + ' seconds', true)
-            .addField('Requested by:', message.author.username, true)
-            .addField('Queue position:', position, true)
-            .addField('URL:', URL, true)
-            .setFooter('Mazbot - Mazurco066')
-          message.channel.sendEmbed(embed).catch(console.error);
-          });
-      }
+      message.channel.send("Música Adicionada a Fila de Reprodução!");
       
       if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
   
